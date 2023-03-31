f5c943f20a96ab3d3c77ac0ff8abbcac3337f370
Maxwell <mxt.developer@gmail.com>
2023-03-31 20:00:04 +0200

    Add repository to config

--- supabase/functions/config/index.ts
+++ supabase/functions/config/index.ts
@@ -2,26 +2,7 @@ import { Router } from 'x/oak';
 import factory from '../_shared/core/factory.ts';
 import { port } from '../_shared/core/utils.ts';
 import { AppContext } from '../_shared/types/core.d.ts';
-
-// get settings from our database or something based on an app version?
-const config = {
-  // only provide the update information for relevant versions
-  theme: 'follow_system',
-  icon: {
-    error: 'https://anitrend.co/media/icons/error.svg',
-    loading: 'https://anitrend.co/media/icons/loading.svg',
-  },
-  release: {
-    version: '2.0.0-alpha40',
-    code: '200000040',
-  },
-  info: {
-    faq: 'https://docs.anitrend.co/faq',
-    patreon: 'https://patreon.com/wax911',
-    discord: 'https://discord.gg/2wzTqnF',
-  },
-  // add config for genres, and anime ids to utilise
-};
+import { config } from './repository/index.ts';
 
 const router = new Router({
   methods: ['GET'],
---
+++ supabase/functions/config/repository/index.ts
@@ -0,0 +1,19 @@
+// get settings from our database or something based on an app version?
+export const config = {
+  // only provide the update information for relevant versions
+  theme: 'follow_system',
+  icon: {
+    error: 'https://anitrend.co/media/icons/error.svg',
+    loading: 'https://anitrend.co/media/icons/loading.svg',
+  },
+  release: {
+    version: '2.0.0-alpha40',
+    code: '200000040',
+  },
+  info: {
+    faq: 'https://docs.anitrend.co/faq',
+    patreon: 'https://patreon.com/wax911',
+    discord: 'https://discord.gg/2wzTqnF',
+  },
+  // add config for genres, and anime ids to utilise
+};
