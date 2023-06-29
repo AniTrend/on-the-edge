3865348415f87952827b2674dec29287db14bb90
Maxwell <mxt.developer@gmail.com>
2023-06-29 18:33:17 +0200

    Remove preview from deployed functions

--- .github/scripts/deploy-all.sh
+++ .github/scripts/deploy-all.sh
@@ -7,7 +7,6 @@ cd ../../
 supabase functions deploy config
 supabase functions deploy news
 supabase functions deploy news-sync
-supabase functions deploy preview
 supabase functions deploy series
 
 cd $CURRENT_DIR
