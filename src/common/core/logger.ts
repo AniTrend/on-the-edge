0e1032dc75a049e93e66cc5955b3de059009cd8c
Maxwell <mxt.developer@gmail.com>
2025-07-25 18:49:51 +0200

    refactor: replace console logging with logger methods in otel module

--- src/common/core/logger.ts
+++ src/common/core/logger.ts
@@ -4,15 +4,6 @@ import { env } from './env.ts';
 import { MinLogLevel } from '../logger/types.ts';
 import { OTelStream } from '../logger/otel-logger.ts';
 
-const consoleLogger = new ConsoleStream()
-  .withFormat(
-    new TokenReplacer()
-      .withFormat('{msg} {metadata}')
-      .withColor(),
-  );
-
-const otelStream = new OTelStream();
-
 const logLevel = (level: MinLogLevel): Level => {
   switch (level) {
     case 'DEBUG':
@@ -32,8 +23,13 @@ const logger = new Logger()
   .withMinLogLevel(
     logLevel(env<MinLogLevel>('MIN_LOG_LEVEL')),
   )
-  .addStream(consoleLogger)
-  .addStream(otelStream);
+  .addStream(new ConsoleStream()
+    .withFormat(
+      new TokenReplacer()
+        .withFormat('{msg} {metadata}')
+        .withColor(),
+    ))
+  .addStream(new OTelStream());
 
 logger.profilingConfig()
   .enabled(env<boolean>('OPTIC_TRACING'))
